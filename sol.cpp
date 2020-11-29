dc7e2cb74bac2177e71c64f999951099d3616760
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-11-29 10:47:37 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		int a[n];
+		for(int i=0; i<n; ++i)
+			cin >> a[i];
+		bool ok=1;
+		for(int i=0; i<n; ++i)
+			if(a[i]%2==0)
+				ok=0;
+		cout << (ok?"YES":"NO") << "\n";
+	}
+}
